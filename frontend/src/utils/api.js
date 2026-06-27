02dba3d0a6c774504af270e88d9d219d2faeb320
UPPALAPATI GEYA MALINI <uppalapatigeyamalini@gmail.com>
2026-06-27 12:46:42 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API = axios.create({ 
   baseURL: typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api'
-    : 'https://event-registration-zvhj.onrender.com'
+    : 'https://event-registration-zvhj.onrender.com/api'
 });
 
 export const setAuthToken = (token) => {
