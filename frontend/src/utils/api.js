3afd12b37c6298332997ca95ff44880f8704713f
Geya Malini Uppalapati <your-github-email@example.com>
2026-08-20 22:05:33 +0530

    Configure frontend for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
-const API = axios.create({ 
-  baseURL: typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-    ? 'http://localhost:5000/api'
-    : 'https://event-registration-zvhj.onrender.com/api'
+
+const API = axios.create({
+  baseURL:
+    typeof window !== 'undefined' &&
+    (window.location.hostname === 'localhost' ||
+      window.location.hostname === '127.0.0.1')
+      ? 'http://localhost:5000/api'
+      : 'https://event-registration-zvhj.onrender.com/api'
 });
 
 export const setAuthToken = (token) => {
-  if (token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  else delete API.defaults.headers.common['Authorization'];
+  if (token) {
+    API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.common['Authorization'];
+  }
 };
 
 export default API;
